f11405b358ba3c52ef56a5ea9a4d5b3471da2bfe
vladyaldy <vladybarinov@gmail.com>
2022-03-17 09:12:19 +0300

    Initial Commit

---
+++ MVPTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPTest1
+//
+//  Created by Vladislav Barinov on 17.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
